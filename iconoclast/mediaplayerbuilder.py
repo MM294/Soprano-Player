697ad7a8513766f5660f89424051cdde71ea1936
Mike Morley <mmorely19@gmail.com>
2011-06-18 18:36:51 +0100

    first commit

---
+++ iconoclast/mediaplayerbuilder.py
@@ -0,0 +1,234 @@
+from gi.repository import Gtk, GdkPixbuf, Gdk, Gio, GObject
+import os
+import gst
+from tagreading import TrackMetaData
+
+def widget_hide(widget, button):
+    widget.hide()
+
+class BuilderApp:
+    def __init__(self):
+	#Gstreamer Bits#
+	self.player = gst.element_factory_make("playbin2", "player")
+	fakesink = gst.element_factory_make("fakesink", "fakesink")
+	self.player.set_property("video-sink", fakesink)
+	#timer to update the time labels and seek bar#
+	self.timer = GObject.timeout_add(500, self.update_time_labels)
+
+	#Window Creation
+        self.builder = Gtk.Builder()
+        filename = os.path.join('', 'MainWindow.glade')        
+
+        self.builder.add_from_file(filename)
+        self.builder.connect_signals(self)	
+
+        self.window = self.builder.get_object('win-main')
+	self.window.set_default_size(900, 550)
+        self.window.connect('destroy', lambda x: Gtk.main_quit())
+        self.window.show_all()
+	#register drag and drop
+	
+        self.treeview = self.builder.get_object('vbox2')
+	self.treeview.drag_dest_set(0, [], 0)
+	self.treeview.connect('drag_motion', self.motion_cb)
+	self.treeview.connect('drag_drop', self.drop_cb)
+	self.treeview.connect('drag_data_received', self.got_data_cb)
+
+	#Quit, About Menus
+	self.menuaqt = self.builder.get_object('menu-quit')
+	self.menuaqt.connect('activate', self.quit_activate)
+
+	self.menuabt = self.builder.get_object('menu-about')
+	self.menuabt.connect('activate', self.about_activate)
+
+	#View Menu#
+	self.menuvfull = self.builder.get_object('menu-mode-full')
+	self.menuvfull.connect('activate', self.to_full_mode)
+
+	self.menuvlean = self.builder.get_object('menu-mode-lean')
+	self.menuvlean.connect('activate', self.to_lean_mode)
+
+	self.menuvmini = self.builder.get_object('menu-mode-mini')
+	self.menuvmini.connect('activate', self.to_mini_mode)
+
+	self.menuvplist = self.builder.get_object('menu-mode-playlist')
+	self.menuvplist.connect('activate', self.to_playlist_mode)
+
+	#playing Toolbar
+	self.toolstop = self.builder.get_object('btn-stop')
+	self.toolstop.connect('clicked', self.stop_play)
+
+	self.toolplay = self.builder.get_object('btn-play')
+	self.toolplay.connect('clicked', self.play_pause)
+
+	self.toolSeekBar = self.builder.get_object('scl-position')
+	self.toolSeekBar.connect('change-value', self.seek)
+
+	self.toolVolume = self.builder.get_object('btn-volume')
+	self.toolVolume.connect('value-changed', self.change_volume)
+
+	#listview#
+	self.listview = self.builder.get_object('treeview1')
+	self.listview.connect('row-activated', self.on_activated)    
+
+    #Math Funcs and Other Handlers#
+
+    def update_time_labels(self):
+	playstate = self.player.get_state()[1]
+	if (playstate == gst.STATE_PLAYING):
+		self.seek_scale_set(None)
+		self.sclSeek = self.builder.get_object('scl-position')
+		self.position = self.player.query_position(gst.FORMAT_TIME, None)[0]
+		self.sclSeek.set_value(self.position)
+
+		self.currTrackPosText = self.convert_ns(self.position)
+		self.timeLabel = self.builder.get_object('lbl-elapsedTime')
+		self.timeLabel.set_text(self.currTrackPosText)
+	return True
+		
+    def convert_ns(self, t):
+	# This method was submitted by Sam Mason.
+	# It's much shorter than the original one.
+	s,ns = divmod(t, 1000000000)
+	m,s = divmod(s, 60)
+
+	if m < 60:
+		return "%02i:%02i" %(m,s)
+	else:
+		h,m = divmod(m, 60)
+		return "%i:%02i:%02i" %(h,m,s)
+
+    def about_activate(self, action):
+        about_dlg = self.builder.get_object('aboutdialog1')
+        about_dlg.show()
+	about_dlg.connect('response', widget_hide)
+
+    def quit_activate(self, action):
+        Gtk.main_quit()
+
+    #Listbox Handlers
+    def add_row(self, action):
+	model = self.builder.get_object('liststore1')
+	action = action.replace('%20',' ')
+	getmesumdatabruv = TrackMetaData()
+	if getmesumdatabruv.getTrackType(action) != False:
+		model.append(getmesumdatabruv.getTrackType(action))
+
+    def on_activated(self, widget, row, col):        
+        model = widget.get_model()
+        text = model[row][6]
+	text = "file://" + text 
+	self.playitem(text)   	
+
+    #Drag and Drop Handling
+    def motion_cb(self, windowid, context, x, y, time):
+	return True
+
+    def drop_cb(self, windowid, context, x, y, time):
+	# Some data was dropped, get the data
+	windowid.drag_get_data(context, context.list_targets()[-1], time)
+	return True
+
+    def got_data_cb(self, windowid, context, x, y, data, info, time):
+	# Got data.
+	tempArray = data.get_text().splitlines()
+	for i in tempArray:
+		i = i.replace('file://','')
+		#print i
+		self.add_row(i)
+	context.finish(True, False, time)
+
+    #View Mode Menu Handlers#
+    def to_full_mode(self, unused):
+	self.builder.get_object('pan-main').get_child1().show()
+	self.builder.get_object('statusbar').show()
+	self.builder.get_object('box-btn-tracklist').show()
+	self.builder.get_object('scrolled-tracklist').show()
+	self.window.resize(900, 550)
+
+    def to_lean_mode(self, unused):
+	self.to_full_mode(None)	
+	self.builder.get_object('box-btn-tracklist').hide()
+
+    def to_mini_mode(self, unused):
+	self.to_full_mode(None)	
+	self.builder.get_object('pan-main').get_child1().hide()
+	self.builder.get_object('statusbar').hide()
+	self.builder.get_object('box-btn-tracklist').hide()
+	self.builder.get_object('scrolled-tracklist').hide()
+	self.window.resize(600, 150)
+
+    def to_playlist_mode(self, unused):
+	self.to_full_mode(None)	
+	self.window.resize(600, 550)
+	self.builder.get_object('pan-main').get_child1().hide()
+
+    #Bottom Toolbar Handlers#
+    def clear_liststore(self, action):
+	model = self.builder.get_object('liststore1')
+	model.clear()    
+
+    #Audio Control Widget Handlers#
+    def change_volume(self, volume, unused):
+	volume = self.builder.get_object('btn-volume').get_value()
+	self.player.set_property('volume', volume)
+
+    def seek_scale_set(self, unused):
+	self.sclSeek = self.builder.get_object('scl-position')
+
+	self.currTrackLength = (self.player.query_duration(gst.FORMAT_TIME, None)[0])
+	self.sclSeek.set_range(0, self.currTrackLength)
+
+	self.currTrackLengthText = self.convert_ns(self.currTrackLength)
+	self.timeLabel = self.builder.get_object('lbl-remainingTime')
+	self.timeLabel.set_text(self.currTrackLengthText)
+
+    def play_pause(self, filepath):
+	selected = self.builder.get_object('treeview-selection1')
+	modeliter = selected.get_selected()[1]
+	model = self.builder.get_object('liststore1')
+	filepath = model.get_value(modeliter, 6)
+
+	toolplayimg = self.builder.get_object('image3')
+	playstate = self.player.get_state()[1]
+	if (playstate == gst.STATE_PLAYING):
+		self.player.set_state(gst.STATE_PAUSED)
+
+		toolplayimg.set_from_icon_name('media-playback-start', Gtk.IconSize.LARGE_TOOLBAR)			
+	elif os.path.isfile(filepath):
+		self.player.set_property("uri", "file://" + filepath)
+		self.player.set_state(gst.STATE_PLAYING)
+
+		toolplayimg.set_from_icon_name('media-playback-pause', Gtk.IconSize.LARGE_TOOLBAR)
+
+    def playitem(self, filepath):
+	self.player.set_state(gst.STATE_NULL)
+	self.player.set_property("uri", filepath)
+	self.player.set_state(gst.STATE_PLAYING)
+
+	toolplayimg = self.builder.get_object('image3')
+	toolplayimg.set_from_icon_name('media-playback-pause', Gtk.IconSize.LARGE_TOOLBAR)	
+
+    def stop_play(self, unused):
+	self.player.set_state(gst.STATE_NULL)
+
+	toolplayimg = self.builder.get_object('image3')
+	toolplayimg.set_from_icon_name('media-playback-start', Gtk.IconSize.LARGE_TOOLBAR)
+
+    def seek(self, widget, test, where):
+        self.player.seek_simple(gst.FORMAT_TIME, gst.SEEK_FLAG_FLUSH, where)
+	self.update_time_labels()
+
+def main(iconoclast=None):
+    app = BuilderApp()
+    #default to playlist till i fix other shit#
+    app.to_playlist_mode(None)      
+
+    #bottom toolbar
+    barclr = app.builder.get_object('btn-tracklistClear')
+    barclr.connect('clicked', app.clear_liststore)
+
+    if __name__ == '__main__':
+    	Gtk.main()
+
+main()
---
+++ iconoclast/tagreading.py
@@ -0,0 +1,143 @@
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3
+from mutagen.mp4 import MP4
+from mutagen.oggvorbis import OggVorbis
+from mutagen.flac import FLAC
+from mutagen.asf import ASF
+
+from os.path import splitext
+
+class TrackMetaData:
+	def getTrackType(self, filepath):
+		fileExtension = splitext(filepath.lower())[1]
+		if fileExtension == '.ogg' or fileExtension == '.oga':
+			return self.oggInfo(filepath)
+		elif fileExtension == '.mp4' or fileExtension == '.m4a':
+			return self.m4aInfo(filepath)
+		elif fileExtension == '.mp2' or fileExtension == '.mp3':
+			return self.id3Info(filepath)
+		elif fileExtension == '.flac':
+			return self.flacInfo(filepath)
+		elif fileExtension == '.wma':
+			return self.wmaInfo(filepath)
+		else:
+			print "not a valid file extension!"
+			return False
+		
+	def id3Info(self, filepath):
+		audio = ID3(filepath)
+
+		try: tracknum = audio["TRCK"][0]
+		except: tracknum = None
+		if tracknum is not None:
+			try:    tracknum = int(tracknum.split('/')[0])
+			except: tracknum = int(tracknum)
+		try: songtitle = audio["TIT2"][0]
+		except: songtitle = None
+		try: artist = audio["TPE1"][0]
+		except: artist = None
+		try: album = audio["TALB"][0]
+		except: album = None
+		try: genre = audio["TCON"][0]
+		except: genre = None
+
+		tracklength = int(round(MP3(filepath).info.length))
+		m,s = divmod(tracklength, 60)
+		tracklength = "%02i:%02i" %(m,s)
+
+		return tracknum, songtitle, artist, album, tracklength, genre, filepath
+
+	def m4aInfo(self, filepath):
+		audio = MP4(filepath)
+
+		try: tracknum = audio["trkn"][0][0]
+		except: tracknum = None
+		if tracknum is not None:
+			try:    tracknum = int(tracknum.split('/')[0])
+			except: tracknum = int(tracknum)
+		try: songtitle = audio["\xa9nam"][0]
+		except: songtitle = None
+		try: artist = audio["\xa9ART"][0]
+		except: artist = None
+		try: album = audio["\xa9alb"][0]
+		except: album = None
+		try: genre = audio["\xa9gen"][0]
+		except: genre = None
+
+		tracklength = int(round(audio.info.length))
+		m,s = divmod(tracklength, 60)
+		tracklength = "%02i:%02i" %(m,s)
+
+		return tracknum, songtitle, artist, album, tracklength, genre, filepath
+
+	def oggInfo(self, filepath):
+		audio = OggVorbis(filepath)
+
+		try: tracknum = audio["tracknumber"][0] 
+		except: tracknum = None
+		if tracknum is not None:
+			try:    tracknum = int(tracknum.split('/')[0])
+			except: tracknum = int(tracknum)
+		try: songtitle = audio["title"][0]
+		except: songtitle = None
+		try: artist = audio["artist"][0]
+		except: artist = None
+		try: album = audio["album"][0]
+		except: album = None
+		try: genre = audio["genre"][0]
+		except: genre = None
+
+		tracklength = int(round(audio.info.length))
+		m,s = divmod(tracklength, 60)
+		tracklength = "%02i:%02i" %(m,s)
+
+		return tracknum, songtitle, artist, album, tracklength, genre, filepath
+
+	def flacInfo(self, filepath):
+		audio = FLAC(filepath)
+
+		try: tracknum = audio["tracknumber"][0] 
+		except: tracknum = None
+		if tracknum is not None:
+			try:    tracknum = int(tracknum.split('/')[0])
+			except: tracknum = int(tracknum)
+		try: songtitle = audio["title"][0]
+		except: songtitle = None
+		try: artist = audio["artist"][0]
+		except: artist = None
+		try: album = audio["album"][0]
+		except: album = None
+		try: genre = audio["genre"][0]
+		except: genre = None
+
+		tracklength = int(round(audio.info.length))
+		m,s = divmod(tracklength, 60)
+		tracklength = "%02i:%02i" %(m,s)
+
+		return tracknum, songtitle, artist, album, tracklength, genre, filepath
+
+	def wmaInfo(self, filepath):
+		audio = ASF(filepath)
+
+		try: tracknum = audio["WM/TrackNumber"][0]
+		except: tracknum = None
+		if tracknum is not None:
+			try:    tracknum = int(tracknum.split('/')[0])
+			except: tracknum = int(tracknum)
+		try: songtitle = str(audio["Title"][0])
+		except: songtitle = None
+		try: artist = str(audio["Author"][0])
+		except: artist = None
+		try: album = str(audio["WM/AlbumTitle"][0])
+		except: album = None
+		try: genre = str(audio["WM/Genre"][0])
+		except: genre = None
+
+		tracklength = int(round(audio.info.length))
+		m,s = divmod(tracklength, 60)
+		tracklength = "%02i:%02i" %(m,s)
+
+		return tracknum, songtitle, artist, album, tracklength, genre, filepath
+
+#getmesumdatabruv = TrackMetaData()
+#print(getmesumdatabruv.getTrackType("/media/Media/Music/Carlos Santana/Playin With Carlos/02-carlos_santana-too_late_too_late_(feat_gregg_rolie).mp3"))
